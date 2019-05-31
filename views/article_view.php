9f2d59a8d19c41b1c3ae1fd1080d984a2c748efe
Nible <enzo.guilmer@gmail.com>
2019-05-31 18:56:46 +0200

    update img

--- views/article_view.php
+++ views/article_view.php
@@ -39,7 +39,6 @@
             <?php if($Article['latex']==0){
                     $Parsedown = new Parsedown();
                     $text = $Parsedown->text($Article['article']);
-                    // echo $text;
                     echo str_replace("&gt;",">",str_replace("&quot;","\"",str_replace("&amp;","&",str_replace("&lt;","<",$text))));
                 }elseif($Article['latex']==1){
                     $Parsedown = new ParsedownExtensionMathJaxLaTeX();
